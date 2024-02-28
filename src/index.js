fffc7eb68d7f995b8f4fb403588c34f8d0f81315
Chandrasekar <chandrasekar51200@gmail.com>
2024-02-28 12:52:30 +0530

    Made some changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="container">
+      <section className="firstHalf">
+        <Avatar />
+        <Content />
+      </section>
+      <SkillList />
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatarImage"
+      src="avatar-jessica.jpeg"
+      alt="avatar for social link"
+    />
+  );
+}
+
+function Content() {
+  return (
+    <>
+      <h3 className="name">Jessica Randall</h3>
+      <p className="location">London, United Kingdom</p>
+      <p className="description">"Front-end developer and avid reader"</p>
+    </>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill={"Github"} />
+      <Skill skill={"Frontend Mentor"} />
+      <Skill skill={"LinkedIn"} />
+      <Skill skill={"Twitter"} />
+      <Skill skill={"Instagram"} />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill">
+      <p>{props.skill}</p>
+    </div>
+  );
+}
+
+const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
